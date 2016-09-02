3474578399ffa90f481b43ad945ff94f36b7b550
chefang <1522815630@qq.com>
2016-09-02 17:49:23 +0800

    chain of responsibility pattern

---
+++ SimpleFactoryPattern/ChainOfResponsibilityPattern/main/java/FileLogger.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class FileLogger extends AbstractLogger {
+
+	public FileLogger(int level) {
+		this.level = level;
+	}
+	
+	@Override
+	protected void write(String message) {
+		System.out.println("FileLogger::Logger" + message);
+	}
+
+}
