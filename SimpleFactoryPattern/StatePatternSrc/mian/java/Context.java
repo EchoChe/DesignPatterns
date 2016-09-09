206cade826322f41bc98956ffbf93fc8331da5ed
chefang <1522815630@qq.com>
2016-09-09 16:06:45 +0800

    State Pattern

---
+++ SimpleFactoryPattern/StatePatternSrc/mian/java/Context.java
@@ -0,0 +1,18 @@
+package mian.java;
+
+public class Context {
+	private State state;
+	
+	public Context() {
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	
+}
---
+++ SimpleFactoryPattern/StatePatternSrc/mian/java/State.java
@@ -0,0 +1,5 @@
+package mian.java;
+
+public interface State {
+	public void doAction(Context context) ;
+}
